6ee8484bee84952a8b701af1a435dccf0b762236
tranthien87 <52022733+tranthien87@users.noreply.github.com>
2024-05-23 09:18:01 +0000

    update nack

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 'use strict'
 
-const {consumeToQueue} = require('./src/services/consumerQueue.service');
+const {consumeToQueue, consumeToQueueSuccess, consumeToQueueDead} = require('./src/services/consumerQueue.service');
 
 const queueName = 'test-queue';
 
-consumeToQueue(queueName).then(() => {
-    console.log('Message consumer started:::', queueName);
+// consumeToQueue(queueName).then(() => {
+//     console.log('Message consumer started:::', queueName);
+// }).catch(error => console.error('Error server::', error));
+
+consumeToQueueSuccess().then(() => {
+    console.log('Message consumer success  started:::');
+}).catch(error => console.error('Error server::', error))
+
+
+
+consumeToQueueDead().then(() => {
+    console.log('Message consumer failed started:::');
 }).catch(error => console.error('Error server::', error))
--- src/services/consumerQueue.service.js
+++ src/services/consumerQueue.service.js
@@ -9,6 +9,68 @@ const consumeService = {
        } catch (error) {
          console.error('Error comsumeToQueue', error)
        }
+    },
+    consumeToQueueSuccess: async () => {
+      try {
+        const { channel, connection } = await connectToRBMQ();
+        const notiQueue = 'notificationQueueProcess';
+        // ttl
+        // const timeExpire = 15000;
+        // setTimeout( () => {
+        //    channel.consume(notiQueue, msg => {
+        //     console.log(`Message sent successfull:: ${msg.content.toString()}`);
+        //     channel.ack(msg)
+        //   })
+        // }, timeExpire)
+       
+        channel.consume(notiQueue, msg => {
+          try {
+            const random = Math.random();
+            console.log(random);
+            if(random < 0.8 ) {
+              throw Error("Random too  big")
+            }
+            console.log(`Message sent successfull:: ${msg.content.toString()}`);
+            channel.ack(msg)
+          } catch (error) {
+            channel.nack(msg, false, false )
+          }
+         
+        })
+       
+      } catch (error) {
+        console.error(error);
+        throw error;
+      
+      }
+    },
+    consumeToQueueDead: async () => {
+      try {
+        const { channel, connection } = await connectToRBMQ();
+
+        const notificationExchangeDLX = 'notificationExDLX';
+        const notificationRoutingKeyDLX = 'notificationRoutingKeyDLX';
+
+        const notificationHotFix ='notificationQueueHotFix';
+
+        await channel.assertExchange(notificationExchangeDLX, 'direct', {
+          durable: true
+        })
+        const queueResult = await channel.assertQueue(notificationHotFix, {
+          exclusive: false
+        })
+        await channel.bindQueue(queueResult.queue, notificationExchangeDLX, notificationRoutingKeyDLX )
+        
+        await channel.consume(queueResult.queue, msgFailed => {
+          console.log(`This notification info hot fix:: ${msgFailed.content.toString()}`);
+        }, 
+        { 
+          noAck: true
+        })
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
     }
 }
 module.exports = consumeService;
